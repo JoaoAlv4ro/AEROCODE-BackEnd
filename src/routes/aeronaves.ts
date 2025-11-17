916b17968803ad99c207614f3d5cedd21ed4ead6
João Álvaro <joaosilvaabc1@gmail.com>
2025-11-17 10:46:08 -0300

    ✨ feat: end-points Aeronaves

---
+++ src/routes/aeronaves.ts
@@ -0,0 +1,50 @@
+import { Router } from "express";
+import { prisma } from "../lib/prisma";
+import { error } from "console";
+
+export const aeronavesRoutes = Router();
+
+aeronavesRoutes.get('/', async (_req, res) => {
+    const data = await prisma.aeronave.findMany();
+    res.status(200).json(data);
+});
+
+aeronavesRoutes.get('/:codigo', async (req, res) => {
+    const { codigo } = req.params;
+    const data = await prisma.aeronave.findUnique({ where: { codigo } });
+    if (!data) return res.status(404).json({ error: 'Aeronave not found' });
+    res.status(200).json(data);
+});
+
+aeronavesRoutes.post('/', async (req, res) => {
+    try { 
+        const created = await prisma.aeronave.create({ data: req.body });
+        res.status(201).json(created);
+    } catch (e: any) {
+        res.status(400).json({ error: e.message });
+    }
+});
+
+aeronavesRoutes.put('/:codigo', async (req, res) => {
+    const { codigo } = req.params;
+    try {
+        const updated = await prisma.aeronave.update({
+            where: { codigo },
+            data: req.body
+        });
+        res.status(200).json(updated);
+    } catch {
+        res.status(404).json({ error: 'Aeronave não encontrada' });
+    }
+});
+
+aeronavesRoutes.delete('/:codigo', async (req, res) => {
+    const { codigo } = req.params;
+    try {
+        await prisma.aeronave.delete({ where: { codigo } });
+        res.status(204).send();
+    } catch {
+        res.status(404).json({ error: 'Aeronave não encontrada' });
+    }
+});
+
